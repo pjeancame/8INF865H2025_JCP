323a9c9806f67492c1e05e1dc7926934e9220943
pjeancame <pjeancame@gmail.com>
2025-02-07 22:47:04 -0500

    Birthday part 2 Etape 3

--- app/src/main/java/com/jcp/happybirthday/MainActivity.kt
+++ app/src/main/java/com/jcp/happybirthday/MainActivity.kt
@@ -17,11 +17,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.jcp.happybirthday.ui.theme.HappyBirthdayTheme
+import androidx.compose.foundation.Image
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,10 +68,22 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.androidparty)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Sam!", from = "From Jean Came")
+        GreetingImage(
+            message = "Happy Birtday Sam!",
+            from = "From Jean Came"
+        )
     }
 }
