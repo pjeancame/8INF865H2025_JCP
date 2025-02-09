4b2531ec67e1090e17e95caa9969f125ef505257
pjeancame <pjeancame@gmail.com>
2025-02-08 20:59:55 -0500

    TipTime Step 10

--- app/src/main/java/com/jcp/tiptime/MainActivity.kt
+++ app/src/main/java/com/jcp/tiptime/MainActivity.kt
@@ -55,6 +55,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeLayout(){
+    var amountInput by remember {mutableStateOf("")}
+
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip  = calculateTip(amount)
+
     Column(
         // Comment
         modifier = Modifier
@@ -71,9 +76,15 @@ fun TipTimeLayout(){
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-        EditNumberField(modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+        EditNumberField(
+            value = amountInput,
+            onValueChange = {amountInput = it},
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+        )
         Text (
-            text = stringResource(R.string.tip_amount, "$0.00"),
+            text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
@@ -95,11 +106,15 @@ fun TipTimeLayoutPreview(){
 
 // More comments hhh
 @Composable
-fun EditNumberField(modifier: Modifier = Modifier){
-    var amountImput by remember {mutableStateOf("")}
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
+
     TextField(
-        value = amountImput,
-        onValueChange = { amountImput = it},
+        value = value,
+        onValueChange = onValueChange,
         label = { Text(stringResource(R.string.bill_amount))},
         singleLine = true,
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
