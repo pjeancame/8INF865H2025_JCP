c83af016c9c66c97999554bce240efa7cf8ffde8
pjeancame <pjeancame@gmail.com>
2025-02-08 22:54:01 -0500

    TipTime part 2  Step 6

--- app/src/main/java/com/jcp/tiptime/MainActivity.kt
+++ app/src/main/java/com/jcp/tiptime/MainActivity.kt
@@ -36,6 +36,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.annotation.StringRes
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Switch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,10 +61,11 @@ class MainActivity : ComponentActivity() {
 fun TipTimeLayout(){
     var amountInput by remember {mutableStateOf("")}
     var tipInput by remember { mutableStateOf("") }
+    var roundUp by remember { mutableStateOf(false) }
 
     val amount = amountInput.toDoubleOrNull() ?: 0.0
     val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
-    val tip  = calculateTip(amount, tipPercent)
+    val tip  = calculateTip(amount, tipPercent, roundUp)
 
     Column(
         // Comment
@@ -102,6 +107,11 @@ fun TipTimeLayout(){
                 .padding(bottom = 32.dp)
                 .fillMaxWidth()
         )
+        RoundTheTipRow(
+            roundUp = roundUp,
+            onRoundUpChanged = { roundUp = it },
+            modifier = Modifier.padding(bottom = 32.dp)
+        )
         Text (
             text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
@@ -110,8 +120,15 @@ fun TipTimeLayout(){
     }
 }
 
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String{
-    val tip = tipPercent/100*amount
+private fun calculateTip(
+    amount: Double,
+    tipPercent: Double = 15.0,
+    roundUp: Boolean
+): String{
+    var tip = tipPercent/100*amount
+    if(roundUp){
+        tip = kotlin.math.ceil(tip)
+    }
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
@@ -123,7 +140,7 @@ fun TipTimeLayoutPreview(){
     }
 }
 
-// More comments hhh
+// Champs numerique
 @Composable
 fun EditNumberField(
     @StringRes label: Int,
@@ -143,5 +160,29 @@ fun EditNumberField(
         modifier = modifier,
     )
 }
+
+@Composable
+fun RoundTheTipRow(
+    roundUp: Boolean,
+    onRoundUpChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+){
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(48.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = stringResource(R.string.round_up_tip))
+        Switch(
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End),
+            checked = roundUp,
+            onCheckedChange = onRoundUpChanged,
+        )
+    }
+}
+
 
 
